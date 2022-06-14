ccc6f2d652006522cdfee33f039b3e5fbc4467be
liyue <liyue326326@gmail.com>
2022-06-14 19:49:09 +0800

    fix: package
    Signed-off-by: liyue <liyue326326@gmail.com>

--- build/webpack.dll.config.js
+++ build/webpack.dll.config.js
@@ -1,9 +1,9 @@
-const path    = require('path');
+const path = require('path');
 const webpack = require('webpack');
 const config = require('../config');
 module.exports = {
   entry: {
-      vendor: ['vue-router','vuex','vue/dist/vue.common.js','vue/dist/vue.js','vue','aes-js','echarts','lodash','node-forge','vue2-ace-bind']
+    vendor: ['vue-router', 'vuex', 'vue/dist/vue.common.js', 'vue/dist/vue.js', 'vue', 'echarts', 'lodash', 'node-forge', 'vue2-ace-bind', 'countup.js', 'diff', 'js-base64', 'js-yaml', 'jsencrypt', 'moment', 'qs', 'xterm','xterm-addon-attach','xterm-addon-fit','xterm-addon-search','xlsx','vue-virtual-scroll-list']
   },
   output: {
     path: path.join(__dirname, '../static/js'),
