4a36a9488fa681721a8ae4bda4835d3b6b56869e
leozhang2018 <leozhang2018@gmail.com>
2022-06-15 11:18:48 +0800

    fix(typo):change typo of chunk name.
    Signed-off-by: leozhang2018 <leozhang2018@gmail.com>

--- src/router/index.js
+++ src/router/index.js
@@ -573,7 +573,7 @@ const routes = [
       },
       {
         path: 'projects/initialize/:project_name',
-        component: () => import(/* webpackChunkName: "project" */ '@/components/projects/detail_ope/initialize.vue'),
+        component: () => import(/* webpackChunkName: "Project Init" */ '@/components/projects/detail_ope/initialize.vue'),
         meta: {
           requiresAuth: true,
           title: '项目详情'
@@ -621,7 +621,7 @@ const routes = [
           },
           {
             path: 'externalConfig',
-            component: () => import(/* webpackChunkName: "project-external-env" */ '@/components/projects/env/hostEnv/editExternalConfig.vue'),
+            component: () => import(/* webpackChunkName: "Project External Env" */ '@/components/projects/env/hostEnv/editExternalConfig.vue'),
             meta: {
               requiresAuth: true,
               title: '配置托管环境'
@@ -655,7 +655,7 @@ const routes = [
       },
       {
         path: 'projects/detail/:project_name/rbac',
-        component: () => import(/* webpackChunkName: "Project Rbac" */ '@/components/projects/rbac/home.vue'),
+        component: () => import(/* webpackChunkName: "Project RBAC" */ '@/components/projects/rbac/home.vue'),
         meta: {
           requiresAuth: true,
           title: '权限管理'
@@ -663,7 +663,7 @@ const routes = [
       },
       {
         path: 'projects/detail/:project_name/policy',
-        component: () => import(/* webpackChunkName: "project-policy" */ '@/components/projects/policy/home.vue'),
+        component: () => import(/* webpackChunkName: "Project Policy" */ '@/components/projects/policy/home.vue'),
         meta: {
           requiresAuth: true,
           title: '协作模式'
