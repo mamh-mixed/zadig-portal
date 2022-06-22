d0102b8d255dac64d52dbef4bee4c79822a20b4f
Jody <qizhaodi@koderover.com>
2022-06-22 15:47:37 +0800

    fix: deliverable workflow clone has no mirror information
    Signed-off-by: Jody <qizhaodi@koderover.com>

--- src/components/projects/workflow/common/runWorkflow.vue
+++ src/components/projects/workflow/common/runWorkflow.vue
@@ -391,11 +391,11 @@ export default {
         }])
       }
     },
-    getPresetInfo (projectNameAndEnvName) {
+    getPresetInfo (projectNameAndEnvName, initArtifactImage = true) {
       const [, namespace] = projectNameAndEnvName.split(' / ')
       this.precreateLoading = true
 
-      if (this.currentProjectEnvs.length && this.$refs.k8sArtifactRef) {
+      if (initArtifactImage && this.currentProjectEnvs.length && this.$refs.k8sArtifactRef) {
         const registryId = this.currentProjectEnvs.find(env => env.name === namespace).registry_id
         this.$refs.k8sArtifactRef.changeRegistryId(registryId)
       }
@@ -673,7 +673,7 @@ export default {
       if (this.haveForcedInput) {
         const projectName = this.forcedUserInput.product_tmpl_name
         const envName = this.forcedUserInput.namespace
-        this.getPresetInfo(`${projectName} / ${envName}`)
+        this.getPresetInfo(`${projectName} / ${envName}`, false)
       }
     })
     // Determine the project type and use different types of startup methods
