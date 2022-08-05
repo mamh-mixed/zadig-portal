ff3e2c13b616f5ed945578404452d03fc6b1db84
liyue <liyue326326@gmail.com>
2022-08-05 14:15:07 +0800

    fix: default branch
    Signed-off-by: liyue <liyue326326@gmail.com>

--- src/components/projects/workflow/workflowEditor/productWorkflow/modules/components/buildOperate.vue
+++ src/components/projects/workflow/workflowEditor/productWorkflow/modules/components/buildOperate.vue
@@ -107,7 +107,7 @@ export default {
   },
   mounted () {
     this.value.branch_filter.forEach(item => {
-      item.filter_regexp = '.*'
+      item.filter_regexp = item.filter_regexp || '.*'
       this.checkRegular(item.filter_regexp, item)
     })
   },
