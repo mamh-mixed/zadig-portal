aa2d2ca0eea8af2ef9b57c285952377fe37c9ab0
leozhang2018 <leozhang2018@gmail.com>
2022-08-19 16:00:36 +0800

    fix:fix property undefined.
    Signed-off-by: leozhang2018 <leozhang2018@gmail.com>

--- src/components/projects/workflow/productCustomTaskDetail/deployConsole.vue
+++ src/components/projects/workflow/productCustomTaskDetail/deployConsole.vue
@@ -16,9 +16,9 @@
       <el-col v-if="jobInfo" :span="6">
         <span class="item-desc">
           <i class="el-icon-question"></i>
-          <span v-if="jobInfo.skip_check_run_status">未开启服务状态检测</span>
-          <span v-else-if="!jobInfo.skip_check_run_status && jobInfo.status ==='passed'">服务状态检测通过</span>
-          <span v-else-if="!jobInfo.skip_check_run_status && jobInfo.status ==='failed'">服务状态检测未通过</span>
+          <span v-if="jobInfo.spec.skip_check_run_status">未开启服务状态检测</span>
+          <span v-else-if="!jobInfo.spec.skip_check_run_status && jobInfo.status ==='passed'">服务状态检测通过</span>
+          <span v-else-if="!jobInfo.spec.skip_check_run_status && jobInfo.status ==='failed'">服务状态检测未通过</span>
         </span>
       </el-col>
       <el-col :span="1" class="close">
