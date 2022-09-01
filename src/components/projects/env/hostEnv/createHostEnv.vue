bdd8dd73c6ddb9faf399134554a51bf693592531
leozhang2018 <leozhang2018@gmail.com>
2022-09-01 11:08:12 +0800

    fix:checking permission in env creation button.
    Signed-off-by: leozhang2018 <leozhang2018@gmail.com>

--- src/components/projects/env/hostEnv/createHostEnv.vue
+++ src/components/projects/env/hostEnv/createHostEnv.vue
@@ -3,7 +3,7 @@
     <AddHostEnv ref="addHostEnv" />
     <div class="footer">
       <el-button type="primary" @click="$router.back()" size="medium" plain>取消</el-button>
-      <el-button type="primary" @click="save()" size="medium">立即创建</el-button>
+      <el-button type="primary" v-hasPermi="{projectName: projectName, actions: ['create_environment','production:create_environment'],operator:'or',isBtn:true}" @click="save()" size="medium">立即创建</el-button>
     </div>
   </div>
 </template>
@@ -25,6 +25,11 @@ export default {
           )
         }
       }
+    }
+  },
+  computed: {
+    projectName () {
+      return this.$route.params.project_name
     }
   }
 }
