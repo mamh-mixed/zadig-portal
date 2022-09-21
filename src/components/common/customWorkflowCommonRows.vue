9d93b73a2ab57bb4226f59ad91722aea0b496e59
liyue <liyue326326@gmail.com>
2022-09-21 15:46:14 +0800

    fix: custags max-width
    Signed-off-by: liyue <liyue326326@gmail.com>

--- src/components/common/customWorkflowCommonRows.vue
+++ src/components/common/customWorkflowCommonRows.vue
@@ -86,6 +86,7 @@
         </template>
       </el-table-column>
     </el-table>
+    <div style="color: #666;" v-if="type==='plugin'?!job.isShowPlugin:!job.isShowCommon">无需输入变量</div>
     <el-table
       v-if="type==='plugin'?job.isShowPlugin:job.isShowCommon"
       :data="type === 'plugin' ? job.spec.plugin.inputs.filter(item=>item.isShow) : job.spec.properties.envs.filter(item=>item.isShow)"
