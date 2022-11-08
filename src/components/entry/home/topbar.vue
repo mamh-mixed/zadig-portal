5e6163d5f46109be5cbc959cb6d244d65694844a
leozhang2018 <leozhang2018@gmail.com>
2022-11-08 10:47:16 +0800

    chore:document links as a component.
    Signed-off-by: leozhang2018 <leozhang2018@gmail.com>

--- src/components/entry/home/topbar.vue
+++ src/components/entry/home/topbar.vue
@@ -60,68 +60,7 @@
         </div>
       </div>
       <div class="topbar-container-end">
-        <el-popover placement="bottom" popper-class="help-droplist" trigger="hover">
-          <ul class="dropdown-menu" uib-dropdown-menu>
-            <li>
-              <a href="https://docs.koderover.com/zadig/" target="_blank">
-                <i class="icon el-icon-link"></i>
-                <span>文档站</span>
-              </a>
-            </li>
-            <li>
-              <a href="https://docs.koderover.com/zadig/quick-start/introduction/" target="_blank">
-                <i class="icon el-icon-link"></i>
-                <span>入门</span>
-              </a>
-            </li>
-            <li>
-              <a href="https://www.koderover.com/tutorials/" target="_blank">
-                <i class="icon el-icon-link"></i>
-                <span>最佳实践</span>
-              </a>
-            </li>
-            <li>
-              <a href="https://docs.koderover.com/zadig/workflow/trigger/" target="_blank">
-                <i class="icon el-icon-link"></i>
-                <span>工作流</span>
-              </a>
-            </li>
-            <li>
-              <a href="https://docs.koderover.com/zadig/env/loadbalance/" target="_blank">
-                <i class="icon el-icon-link"></i>
-                <span>环境</span>
-              </a>
-            </li>
-            <li>
-              <a href="https://docs.koderover.com/zadig/project/overview/" target="_blank">
-                <i class="icon el-icon-link"></i>
-                <span>项目管理</span>
-              </a>
-            </li>
-            <li>
-              <a href="https://docs.koderover.com/zadig/delivery/artifact/" target="_blank">
-                <i class="icon el-icon-link"></i>
-                <span>交付中心</span>
-              </a>
-            </li>
-            <li role="separator" class="divider"></li>
-            <li>
-              <a href="https://docs.koderover.com/zadig/settings/codehost/gitlab/" target="_blank">
-                <i class="icon el-icon-link"></i>
-                <span>系统设置</span>
-              </a>
-            </li>
-            <li>
-              <a href="https://docs.koderover.com/zadig/api/usage/" target="_blank">
-                <i class="icon el-icon-link"></i>
-                <span>开发者中心</span>
-              </a>
-            </li>
-          </ul>
-          <span slot="reference">
-            <i class="function-icon iconfont iconvery-teach"></i>
-          </span>
-        </el-popover>
+        <DocLink class="function-icon" />
         <Notification class="function-icon" />
         <div class="nav nav-item-bottom user-profile">
           <el-popover placement="bottom" width="240" popper-class="dropdown-menu" trigger="hover">
@@ -191,6 +130,7 @@
 </template>
 <script>
 import Notification from './common/notification.vue'
+import DocLink from './common/docLink.vue'
 import mixin from '@/mixin/topbarMixin'
 import bus from '@utils/eventBus'
 import { mapState, mapGetters } from 'vuex'
@@ -278,7 +218,8 @@ export default {
     })
   },
   components: {
-    Notification
+    Notification,
+    DocLink
   },
   mixins: [mixin]
 }
@@ -302,51 +243,6 @@ export default {
   .iconmorelist {
     margin-left: 54px;
     color: #c4c4c4;
-  }
-}
-
-.help-droplist {
-  .dropdown-menu {
-    margin: 0 2px;
-    padding: 8px 0;
-    list-style: none;
-
-    .divider {
-      height: 1px;
-      margin: 9px 0;
-      overflow: hidden;
-      background-color: #e5e5e5;
-    }
-
-    li {
-      & > a {
-        display: block;
-        clear: both;
-        padding: 3px 20px;
-        padding: 4px 0 4px 5px;
-        color: #333;
-        font-weight: normal;
-        line-height: 1.42857143;
-        white-space: nowrap;
-
-        .icon {
-          position: relative;
-          margin-right: 5px;
-          font-size: 16px;
-        }
-      }
-
-      &:hover {
-        color: #262626;
-        text-decoration: none;
-        background-color: rgba(0, 102, 255, 0.07);
-        border-radius: 6px;
-
-        & > a {
-          color: @themeColor;
-        }
-      }
-    }
   }
 }
 
