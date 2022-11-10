f0966658b4dc573bfb533cd9ed883aaa2d64c70d
liyue <liyue326326@gmail.com>
2022-11-10 20:15:03 +0800

    fix: placeholder
    Signed-off-by: liyue <liyue326326@gmail.com>

--- src/components/templateLibrary/workflows/components/jobs/jobDeploy.vue
+++ src/components/templateLibrary/workflows/components/jobs/jobDeploy.vue
@@ -6,7 +6,7 @@
       </el-form-item>
       <el-form-item label="环境" :required="job.spec.envType && job.spec.envType !== 'runtime'">
         <el-form-item prop="spec.env" v-if="!job.spec.envType ||job.spec.envType === 'runtime'" class="form-item">
-          <el-input v-model="job.spec.env" size="small" placeholder="请输入"></el-input>
+          <el-input v-model="job.spec.env" size="small" placeholder="请输入环境"></el-input>
         </el-form-item>
         <el-form-item prop="spec.env" required v-if="job.spec.envType === 'fixed'" class="form-item">
           <el-select v-model="job.spec.env" placeholder="请选择" size="small">
