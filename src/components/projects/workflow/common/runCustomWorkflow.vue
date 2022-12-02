9fdef06a3ea4e42a5e102d51168a7263357dc1a9
leozhang2018 <leozhang2018@gmail.com>
2022-12-02 15:37:33 +0800

    fix:fix rollback data not show.
    Signed-off-by: leozhang2018 <leozhang2018@gmail.com>

--- src/components/projects/workflow/common/runCustomWorkflow.vue
+++ src/components/projects/workflow/common/runCustomWorkflow.vue
@@ -470,7 +470,7 @@
               </div>
             </div>
             <div v-if="job.type === 'istio-release'">
-              <div style="margin-bottom: 10px; padding-left: 100px; color: #606266;">
+              <div style="margin-bottom: 10px; color: #606266;">
                 <span>新版本副本数百分比:{{job.spec.replica_percentage}}%</span>
                 <span style="margin-left: 10px;">新版本流量百分比:{{job.spec.weight}}%</span>
               </div>
@@ -517,7 +517,7 @@
               </div>
             </div>
             <div v-if="job.type === 'istio-rollback'">
-              <el-form-item label="选择容器" >
+              <el-form-item label="选择容器">
                 <el-select
                   v-model="job.pickedTargets"
                   filterable
@@ -532,11 +532,23 @@
                   <el-option
                     v-for="(item,index) of job.spec.targets"
                     :key="index"
-                    :label="`${item.container_name}/${item.workload_name}`"
+                    :label="`${item.workload_name}/${item.container_name}`"
                     :value="item"
                   ></el-option>
                 </el-select>
               </el-form-item>
+              <template v-if="job.pickedTargets.length > 0">
+                <span style="color: #606266;">回滚版本信息</span>
+                <el-table :data="job.pickedTargets">
+                  <el-table-column label="容器名称" prop="value">
+                    <template slot-scope="scope">
+                      <span>{{`${scope.row.workload_name}/${scope.row.container_name}`}}</span>
+                    </template>
+                  </el-table-column>
+                  <el-table-column label="镜像名称" prop="image"></el-table-column>
+                  <el-table-column label="副本数量" prop="target_replica"></el-table-column>
+                </el-table>
+              </template>
             </div>
           </el-collapse-item>
         </div>
@@ -1055,7 +1067,9 @@ export default {
               // fromjob
               this.fromJobInfo.pickedTargets.forEach(item => {
                 if (item.update_tag && !item.target_tag) {
-                  this.$message.error(`请填写 ${item.service_name} 中的目标镜像版本`)
+                  this.$message.error(
+                    `请填写 ${item.service_name} 中的目标镜像版本`
+                  )
                   throw Error()
                 }
               })
@@ -1170,12 +1184,13 @@ export default {
     },
     handleContainerChange (val, job) {
       val.forEach(item => {
-        this.getRegistryList([item.service || item.container_name], job.spec.docker_registry_id).then(
-          res => {
-            this.$set(item, 'images', res)
-            this.$forceUpdate()
-          }
-        )
+        this.getRegistryList(
+          [item.service || item.container_name],
+          job.spec.docker_registry_id
+        ).then(res => {
+          this.$set(item, 'images', res)
+          this.$forceUpdate()
+        })
       })
       this.$forceUpdate()
     },
