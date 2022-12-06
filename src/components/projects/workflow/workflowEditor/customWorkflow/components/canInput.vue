2cd505d24a9d45172271e3e1946a07dedc822995
liyue <liyue326326@gmail.com>
2022-12-06 10:35:41 +0800

    fix: ui
    Signed-off-by: liyue <liyue326326@gmail.com>

--- src/components/projects/workflow/workflowEditor/customWorkflow/components/canInput.vue
+++ src/components/projects/workflow/workflowEditor/customWorkflow/components/canInput.vue
@@ -8,14 +8,16 @@
       <i class="icon el-icon-edit" @click="handleInput(true)" v-show="!isCanInput" v-if="from==='ui'&&!disabled"></i>
     </span>
     <span v-else class="input">
-      <el-input
-        v-model="inputValue"
-        type="text"
-        size="mini"
-        :placeholder="placeholder"
-        @focus="handleInput(true)"
-        @blur="handleInput(false)"
-      ></el-input>
+      <el-form-item prop="display_name" :rules="{required: true,message:'请输入工作流名称', trigger: ['blur', 'change']}">
+        <el-input
+          v-model="inputValue"
+          type="text"
+          size="mini"
+          :placeholder="placeholder"
+          @focus="handleInput(true)"
+          @blur="handleInput(false)"
+        ></el-input>
+      </el-form-item>
       <el-button type="text" @click="handleInput(false)" size="mini" class="mg-l8">确定</el-button>
     </span>
   </div>
