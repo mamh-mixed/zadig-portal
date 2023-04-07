96b04132821fefbf529fadff85dd5778520953cd
leozhang2018 <leozhang2018@gmail.com>
2023-04-07 13:49:30 +0800

    Revert "fix:using service_options in webhook trigger."
    This reverts commit 4fe177f1a1e99aed4e9ac37578e186ad36bf1bb1.

--- src/components/projects/workflow/workflowEditor/customWorkflow/components/base/webhookRunConfig.vue
+++ src/components/projects/workflow/workflowEditor/customWorkflow/components/base/webhookRunConfig.vue
@@ -59,7 +59,7 @@
                   @change="handleServiceBuildChange($event,job)"
                 >
                   <el-option
-                    v-for="(service,index) of job.spec.service_options"
+                    v-for="(service,index) of job.spec.service_and_builds"
                     :key="index"
                     :label="`${service.service_module}(${service.service_name})`"
                     :value="service"
@@ -435,20 +435,20 @@ export default {
         stage.jobs.forEach(job => {
           if (job.spec && job.spec.service_and_builds) {
             this.cloneWorkflow.fromJobInfo = cloneDeep(job)
-          }
-          if (job.spec && job.spec.service_options) {
-            job.spec.service_options.forEach(service => {
+            job.spec.service_and_builds.forEach(service => {
               service.key_vals.forEach(item => {
-                if (
-                  item.value.includes('<+fixed>') ||
-                  item.value.includes('{{')
-                ) {
+                if (item.value.includes('fixed') || item.value.includes('{{')) {
                   item.isShow = false
                 } else {
                   item.isShow = true
                 }
               })
               service.value = `${service.service_name}/${service.service_module}`
+              service.key_vals.forEach(key => {
+                // if (key.is_credential) {
+                //   key.value = this.$utils.aesDecrypt(key.value)
+                // }
+              })
             })
           }
           if (job.type === 'zadig-deploy' && job.spec.source === 'runtime') {
