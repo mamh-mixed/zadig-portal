90fc351e5262a3216bb47d76ea158be113f44ad7
leozhang2018 <leozhang2018@gmail.com>
2023-08-16 15:14:22 +0800

    fix:fix validation rule.
    Signed-off-by: leozhang2018 <leozhang2018@gmail.com>

--- src/components/entry/installation.vue
+++ src/components/entry/installation.vue
@@ -108,7 +108,7 @@ export default {
     },
     rules () {
       const validatePhone = (rule, value, callback) => {
-        if (value !== '') {
+        if (value !== null && value !== '') {
           if (
             !/^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$/.test(
               value
